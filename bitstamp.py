5e0723c00b2c6c0d73f0594007b055c9bb7045de
rodrigoracr <hackracr@gmail.com>
2020-11-01 23:36:03 -0300

    Mudando a logica de compra e venda

--- bitstamp.py
+++ bitstamp.py
@@ -25,7 +25,7 @@ def ao_receber_mensagem(ws, mensagem):
     print(price)
     if price > 9000:
         vender()
-    elif price < 8000:
+    elif price < 8100:
         comprar()
     else:
         print('Aguardando')
